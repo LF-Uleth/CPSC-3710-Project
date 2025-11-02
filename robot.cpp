f9fd15a29ff04fed48577add7c2750e3145ff327
LF-Uleth <144730637+LF-Uleth@users.noreply.github.com>
2025-11-02 13:33:11 -0700

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -10,3 +10,19 @@ void drawCube(float width, float height, float depth){
         glutSolidCube(1.0f);
     glPopMatrix();
 }
+
+
+void drawRobot(){
+
+//Draw Torso
+glPushMatrix();
+    glTranslatef(0.0, 0.0, 0.0);
+    drawCube(1.25, 1.5, 0.6);
+glPopMatrix();
+
+
+//Draw Head
+    
+
+    
+}
