08850df348a8923ebc6c3f7f801f3be149215829
LF-Uleth <144730637+LF-Uleth@users.noreply.github.com>
2025-11-04 16:32:01 -0700

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -27,7 +27,32 @@ glPopMatrix();
     
 }
 
-void main(){
-drawRobot();
+//display settings
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(3.0, 3.0, 3.0,   
+              0.0, 0.0, 0.0,   
+              0.0, 1.0, 0.0);
 
+    drawRobot();
+
+    glutSwapBuffers();
+}
+
+// set opengl settings
+void initGL() {
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("OpenGL Robot");
+    initGL();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
